b8b93c7f683716391affd39a5926c309e05d4042
Ghassen69 <ghassenayari2000@gmail.com>
2021-02-03 13:10:46 +0100

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- *main - entry point
- *print message
- *return: success
+/**
+ * main - entry point
+ * print message
+ * return: success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

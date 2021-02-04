9c679bbafa3aaeb4994f3b09b63c1a3f1080c1c3
Ghassen69 <ghassenayari2000@gmail.com>
2021-02-04 16:42:59 +0100

    update

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -13,17 +13,21 @@
 int main(void)
 {
 	int x;
+	int e;
 
 	for (x = '0'; x <= '9'; ++x)
 	{
-	if (x != '0')
+	for (e = '0'; e <= '9'; ++e)
+	{
+	if (x != '0' || e != '0')
 	{
 	putchar(',');
 	putchar(' ');
 	}
 	putchar(x);
+	putchar(e);
 	}
-
+	}
 	putchar('\n');
 	return (0);
 }

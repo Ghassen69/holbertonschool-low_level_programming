3b4b549a805fcb7825021a2f62f654a3cce4679a
Ghassen69 <ghassenayari2000@gmail.com>
2021-02-09 11:16:47 +0100

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _abs - absolute value of an integer
+* @c : parameter
+* Return: success
+*/
+int _abs(int c)
+{
+if (c < 0)
+
+return (c * -1);
+
+else
+
+return (0);
+
+}

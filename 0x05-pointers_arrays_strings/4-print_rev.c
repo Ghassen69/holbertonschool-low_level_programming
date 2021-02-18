44d8fb5b7882aaa954329e76a177d82911a1c45b
Ghassen69 <ghassenayari2000@gmail.com>
2021-02-18 11:04:52 +0100

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_rev - print string in reverse
+* @s: pointer
+*/
+void print_rev(char *s);
+{
+int x;
+x = 0;
+while (str[x] != '\0')
+{
+_putchar(str[x]);
+x--;
+}
+_putchar('\n');
+}

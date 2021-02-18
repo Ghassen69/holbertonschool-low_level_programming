1a8498e052ba98af759a794193d3fd3b958f3916
Ghassen69 <ghassenayari2000@gmail.com>
2021-02-18 13:07:33 +0100

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,19 @@
 * print_rev - print string in reverse
 * @s: pointer
 */
-void print_rev(char *s);
+void print_rev(char *s)
 {
-int x;
-x = 0;
-while (str[x] != '\0')
-{
-_putchar(str[x]);
-x--;
-}
-_putchar('\n');
+	int i, j;
+
+	i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i = i + 1;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(*(s + j));
+	}
+	_putchar('\n');
 }

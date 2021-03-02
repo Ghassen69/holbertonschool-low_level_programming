62a12d08496711bd0a2286657d644613597d0428
Ghassen69 <ghassenayari2000@gmail.com>
2021-03-02 13:29:09 +0100

    upate

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,67 +1,70 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+#include <stdio.h>
+
 /**
-* split - function that counts the words found in a string.
-* @str: string.
-* Return: Number of words found in the string.
+* split_word - function that count the number of words
+* @str: Getting a string
+* Return: Number of words founds in a string delimited by white space
 **/
 
-int split(char *str)
+int split_word(char *str)
 {
-int i = 0, word = 0;
+	int i = 0, words = 0;
 
 	while (str[i] != '\0')
 	{
 		if (str[i] != ' ' && str[i - 1] == ' ')
-			word++;
+			words++;
 		i++;
 	}
-	return (word);
+	return (words);
 }
 
 /**
-* strtow - function that splits a string into words.
-* @str: string.
-* Return: Array of words.
+* strtow - function that splites a string into words.
+* @str: Getting a string
+* Return: Array of words, without white space
 **/
 char **strtow(char *str)
 {
-int word = 0, i = 0, j = 0, k = 0, l = 0, m = 0;
-char **ch;
+	int i = 0, j = 0, b = 0, c = 0, r = 0;
+	int words = 0;
+	char **p;
 
 	if (str == NULL)
-	return (NULL);
-	word = split(str);
-	ch = malloc(1 + word * sizeof(char *));
-		if (ch == NULL)
-		{
 		return (NULL);
-		}
-i = 0, l = 0;
-		for (j = 0; j < word; j++)
-		{
-		k = 0, m = 0;
-			for (; str[i] == ' '; i++)
+	words = split_word(str);
+	p = malloc(1 + words * sizeof(char *));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0, c = 0;
+	for (j = 0; j < words; j++)
+	{
+		b = 0, r = 0;
+		for (; str[i] == ' '; i++)
 			;
-				for (; str[i] != ' '; i++)
-				k++;
-				k = k + 1;
-				ch[j] = malloc(k * sizeof(char));
-		if (ch[j] == NULL)
+		for (; str[i] != ' '; i++)
+			b++;
+		b = b + 1;
+		p[j] = malloc(b * sizeof(char));
+		if (p[j] == NULL)
 		{
-		for (; j >= 0; j--)
-		free(ch[j]);
-		free(ch);
+			for (; j >= 0; j--)
+				free(p[j]);
+			free(p);
 		}
-			for (; str[l] == ' '; l++)
+		for (; str[c] == ' '; c++)
 			;
-				for (; str[l] != ' ' && m <= k; l++)
-				{
-				ch[j][m] = str[l];
-				m++;
-				}
-		ch[j][m] = '\0';
+		for (; str[c] != ' ' && r <= b; c++)
+		{
+			p[j][r] = str[c];
+			r++;
 		}
-return (ch);
+		p[j][r] = '\0';
+	}
+	return (p);
 }
